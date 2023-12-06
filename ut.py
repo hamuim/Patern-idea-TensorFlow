3064d910569a8b3d44c679e99cbe07f6da5a3cc1
hamuim <106325644+hamuim@users.noreply.github.com>
2023-12-06 18:10:51 +0900

    Create ut.py

---
+++ ut.py
@@ -0,0 +1,29 @@
+import rn10
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def get_training_model():
+    n = 2
+    depth =  n * 9 + 2
+    n_blocks = ((depth - 2) // 9) - 1
+
+    inputs = tf.keras.layers.Input(shape=(32, 32, 3))
+
+    x = rn10.stem(inputs)
+
+    x = rn10.learner(x, n_blocks)
+
+    outputs = rn10.classifier(x, 10)
+
+    model = tf.keras.Model(inputs, outputs)
+    
+    return model
+
+def plot_history(history):
+    plt.plot(history.history["loss"], label="Training Loss")
+    plt.plot(history.history["val_loss"], label="Validation Loss")
+    plt.plot(history.history["accuracy"], label="Training Accuracy")
+    plt.plot(history.history["val_accuracy"], label="Validation Accuracy")
+    plt.legend()
+    plt.grid()
+    plt.show()
