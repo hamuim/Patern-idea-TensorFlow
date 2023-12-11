2a731db5b16532a0c8b3627139b9b277d4f0c555
hamuim <106325644+hamuim@users.noreply.github.com>
2023-12-11 17:41:27 +0900

    Update ut.py

--- ut.py
+++ ut.py
@@ -3,40 +3,6 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import os
 import numpy as np
-from tensorflow.keras.applications.resnet50 import ResNet50
-
-def feature_extractor(inputs):
-
-  feature_extractor = tf.keras.applications.resnet.ResNet50(input_shape=(224, 224, 3),
-                                               include_top=False,
-                                               weights='imagenet')(inputs)
-  return feature_extractor
-
-def classifier(inputs):
-    x = tf.keras.layers.GlobalAveragePooling2D()(inputs)
-    x = tf.keras.layers.Flatten()(x)
-    x = tf.keras.layers.Dense(1024, activation="relu")(x)
-    x = tf.keras.layers.Dense(512, activation="relu")(x)
-    x = tf.keras.layers.Dense(10, activation="softmax", name="classification")(x)
-    return x
-
-def final_model(inputs):
-
-    resize = tf.keras.layers.UpSampling2D(size=(7,7))(inputs)
-
-    resnet_feature_extractor = feature_extractor(resize)
-    classification_output = classifier(resnet_feature_extractor)
-
-    return classification_output
-
-
-def get_training_model_t():
-  inputs = tf.keras.layers.Input(shape=(32,32,3))
-  
-  classification_output = final_model(inputs) 
-  model = tf.keras.Model(inputs=inputs, outputs = classification_output)
-
-  return model
 
 def get_training_model():
     n = 3
